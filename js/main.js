79032c5856e931209bc019124cbc9882e977a124
Cecilia Limay <mcecilia.limay@gmail.com>
2016-02-15 17:40:13 -0500

    navegadorfixedopa

--- js/main.js
+++ js/main.js
@@ -12,3 +12,16 @@ $(function() {
     }
   });
 });
+
+
+$(document).ready(function(){
+ 
+  $(window).scroll(function(){
+    if( $(this).scrollTop() > 500 ){
+      $('.navegador').addClass('navegador2');
+    } else {
+      $('.navegador').removeClass('navegador2');
+    }
+  });
+ 
+});
